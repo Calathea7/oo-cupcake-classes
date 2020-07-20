7b925c8f20f8d5a81ccdadbda12c01b6879f5167
Anastasia Correa <correa.anastasia7@gmail.com>
2020-07-20 19:05:18 +0000

    Set up instance methods and attributes

--- desserts.py
+++ desserts.py
@@ -3,13 +3,36 @@
 
 class Cupcake:
     """A cupcake."""
-
+    cache = {}
 
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+    
+    def __init__(self, name, flavor, price, qty=0):
 
+      self.name = name
+      self.flavor = flavor
+      self.price = price
+      self.qty = qty
+
+      self.cache[name] = self
+
+    def add_stock(self, amount):
+
+      self.qty += amount
+
+    def sell(self, amount):
+
+      if self.qty == 0:
+        print('Sorry, these cupcakes are sold out')
+      
+      while self.qty >= amount:
+        self.qty = self.qty - amount
+
+    # @staticmethod
+    # def scale_recipe(ingredients, amount):
 
 if __name__ == '__main__':
     import doctest
